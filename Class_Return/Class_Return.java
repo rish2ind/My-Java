cca8204792dd6686ed5577d884ddf0f589d0a23f
Rishabh Mathur <mathur.rishabh123@gmail.com>
2019-01-27 20:21:28 +0530

    Return type in Class

---
+++ Class_Return/Class_Return.java
@@ -0,0 +1,30 @@
+class Box{
+	double width;
+	double length;
+	double height;
+	
+	double volume(){
+		return width*length*height;
+	}
+}
+
+class BoxFinal{
+		public static void main(String args[]){
+		Box b1 = new Box();
+		Box b2 = new Box();
+		double vol;
+				
+		b1.width = 10;
+		b1.length = 3;
+		b1.height = 2;
+		
+		b2.width = 10;
+		b2.length = 5;
+		b2.height = 2;
+		
+		vol = b1.volume(); 
+		System.out.println("Volume of Box 1 is : " + vol);
+		vol = b2.volume(); 
+		System.out.println("Volume of Box 2 is : " + vol);
+	}
+}
